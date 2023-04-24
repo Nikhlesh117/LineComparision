df410fd0d52aeccea3e80af7033eddb99f18692a
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-24 09:43:53 +0530

    Added: Line Compare

--- ConsoleApp1/ConsoleApp1/CalculateLine.cs
+++ ConsoleApp1/ConsoleApp1/CalculateLine.cs
@@ -6,22 +6,28 @@ using System.Threading.Tasks;
 
 namespace LineComparision
 {
-    public class CalculateLine
+    namespace LineComparision
     {
-        double x_One, y_One, x_Two, y_Two;
-        public CalculateLine(double x_One, double y_One, double x_Two, double y_Two)
+        public class CalculateLine
         {
-            this.x_One = x_One;
-            this.y_One = y_One;
-            this.x_Two = x_Two;
-            this.y_Two = y_Two;
-        }
+            double x_One, y_One, x_Two, y_Two;
+            public CalculateLine(double x_One, double y_One, double x_Two, double y_Two)
+            {
+                this.x_One = x_One;
+                this.y_One = y_One;
+                this.x_Two = x_Two;
+                this.y_Two = y_Two;
+            }
 
-        public void LengthLine()
-        {
-            double Line = Math.Sqrt(Math.Pow(x_Two - x_One, 2) + Math.Pow(y_Two - y_One, 2));
-            Console.WriteLine("Line Length is {0}", Line);
-        }
+            public double LengthLine()
+            {
+                double Line = Math.Sqrt(Math.Pow(x_Two - x_One, 2) + Math.Pow(y_Two - y_One, 2));
+                Console.WriteLine("Line Length is {0}", Line);
+                return Line;
+            }
 
+        }
     }
+
+   
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,38 @@
-﻿namespace LineComparision
+﻿using LineComparision.LineComparision;
+
+namespace LineComparision
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             CalculateLine obj = new CalculateLine(10, 20, 30, 40);
-            obj.LengthLine();
+            double Line_one = obj.LengthLine();
+            CalculateLine obj_two = new CalculateLine(20, 40, 60, 80);
+            double Line_Two = obj_two.LengthLine();
+
+            if (Line_one.Equals(Line_Two))
+            {
+                Console.WriteLine("Both the Line are Equal");
+            }
+            else
+            {
+                Console.WriteLine("NOT Equal");
+            }
+
+
+            if(Line_one.CompareTo(Line_Two)==0)
+            {
+                Console.WriteLine("Both Line are Equal");
+            }
+            else if(Line_one.CompareTo(Line_Two) >0 )
+            {
+                Console.WriteLine("Line One is greater than line two");
+            }
+            else
+            {
+                Console.WriteLine("Line Two is greater than line One");
+            }
         }
     }
 }
