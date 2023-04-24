e7c379e0026c692f9c47c992a3eaa3402f25dab6
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-24 08:55:47 +0530

    Added: Calculate Line

---
+++ ConsoleApp1/ConsoleApp1/CalculateLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class CalculateLine
+    {
+        double x_One, y_One, x_Two, y_Two;
+        public CalculateLine(double x_One, double y_One, double x_Two, double y_Two)
+        {
+            this.x_One = x_One;
+            this.y_One = y_One;
+            this.x_Two = x_Two;
+            this.y_Two = y_Two;
+        }
+
+        public void LengthLine()
+        {
+            double Line = Math.Sqrt(Math.Pow(x_Two - x_One, 2) + Math.Pow(y_Two - y_One, 2));
+            Console.WriteLine("Line Length is {0}", Line);
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparision
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculateLine obj = new CalculateLine(10, 20, 30, 40);
+            obj.LengthLine();
+        }
+    }
+}
