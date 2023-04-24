de40ac6ee38858f6ef5ddfdba940db790e6d69b9
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-24 09:25:02 +0530

    Added: Line Compare

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,19 @@
         public static void Main(string[] args)
         {
             CalculateLine obj = new CalculateLine(10, 20, 30, 40);
-            obj.LengthLine();
+            double Line_one = obj.LengthLine();
+            CalculateLine obj_two = new CalculateLine(20, 40, 60, 80);
+            double Line_Two = obj_two.LengthLine();
+
+            if(Line_one.Equals(Line_Two))
+            {
+                Console.WriteLine("Both the Line are Equal");
+            }
+            else
+            {
+                Console.WriteLine("NOT Equal");
+            }
+
         }
     }
 }
