df17368a7125ceed79b16b5cbf0942e8b2952aeb
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-04-30 09:06:21 +0530

    Added: LineCompare

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,17 +10,7 @@ namespace LineComparision
             double Line_one = obj.LengthLine();
             CalculateLine obj_two = new CalculateLine(20, 40, 60, 80);
             double Line_Two = obj_two.LengthLine();
-
-            if (Line_one.Equals(Line_Two))
-            {
-                Console.WriteLine("Both the Line are Equal");
-            }
-            else
-            {
-                Console.WriteLine("NOT Equal");
-            }
-
-
+          
             if(Line_one.CompareTo(Line_Two)==0)
             {
                 Console.WriteLine("Both Line are Equal");
